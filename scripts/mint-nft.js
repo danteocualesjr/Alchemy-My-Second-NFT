b43ddd0b2459708be65fc09d2673ea39b095f79d
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-16 20:42:57 +0800

    Updated mint-nft.json

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,5 +1,28 @@
-const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
-console.log(JSON.stringify(contract.abi));
+require('dotenv').config();
+const API_URL = process.env.API_URL;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(API_URL);
+
+const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
 const contractAddress = "0x69fedD2cC68be88574B5C0dcc6b39a9fcC7e082c";
 const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+
+async function mintNFT(tokenURI) {
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+
+  //The transaction
+  const tx = {
+    'from': PUBLIC_KEY,
+    'to': contractAddress,
+    'nonce': nonce,
+    'gas': 500000,
+    'maxPriorityFeePerGas': 1999999987,
+    'data': nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI()
+  };
+}​
+
+// const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+// console.log(JSON.stringify(contract.abi));
