0d9d5ee29b7c377ceda78954e9f98553356ba86f
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-16 20:39:02 +0800

    Updated mint-nft.js

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,2 +1,5 @@
 const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
 console.log(JSON.stringify(contract.abi));
+
+const contractAddress = "0x69fedD2cC68be88574B5C0dcc6b39a9fcC7e082c";
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
