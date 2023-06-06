0c0d241614668fef58a1539b6f0eaf3f6218cf6e
Dante O. Cuales <danteocualesjr@gmail.com>
2023-06-07 07:16:35 +0800

    Refactored code

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 
 const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
    solidity: "0.7.3",
    defaultNetwork: "ropsten",
