b5173ce4e522e6a6484c9014e5d84e3baaf27e5f
Dante O. Cuales <danteocualesjr@gmail.com>
2023-06-10 17:12:21 +0800

    Refactored code

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,8 +11,9 @@ const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
    
    solidity: "0.7.3",
-   
+
    defaultNetwork: "ropsten",
+   
    networks: {
       hardhat: {},
       ropsten: {
